afacc321a36a5d6a14c653e114848c33bf363cae
Sean Genabe <seangenabe@outlook.com>
2016-03-17 14:00:50 +0800

    test for kata to hira transform

---
+++ test/kata-to-hira-transform.test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const test = require('ava')
+
+const TransformedString = require('../lib/transformed-string')
+const KataToHiraTransform = require('../lib/transforms/kata-to-hira-transform')
+const TransformedCharacter = require('../lib/transformed-character')
+const RemoveBees = require('./remove-bees')
+
+test('should transform ok by default', t => {
+  let r = new KataToHiraTransform()
+  let x = new TransformedString('アカサタナいハマヤラワヰヱン', [r])
+  t.is(x.value, 'あかさたないはまやらわゐゑん')
+  t.same(
+    x.items.map(tc => tc.weights.get(r)),
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+  )
+})
+
+test('should expect null', t => {
+  let x = new TransformedString('abc', [new RemoveBees(), new KataToHiraTransform()])
+  t.is(x.value, 'ac')
+})
