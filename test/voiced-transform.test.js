6273809e7f14be1604d9c7d4702c8a039ca8db95
Sean Genabe <seangenabe@outlook.com>
2016-03-17 14:27:01 +0800

    test for voiced transform

--- test/voiced-transform.test.js
+++ test/voiced-transform.test.js
@@ -3,24 +3,24 @@
 const test = require('ava')
 
 const TransformedString = require('../lib/transformed-string')
-const HalfVoicedTransform = require('../lib/transforms/half-voiced-transform')
+const VoicedTransform = require('../lib/transforms/voiced-transform')
 const TransformedCharacter = require('../lib/transformed-character')
 const RemoveBees = require('./remove-bees')
 
 test('should transform ok by default', t => {
-  let r = new HalfVoicedTransform()
+  let r = new VoicedTransform()
   let x = new TransformedString('あがはばぱ', [r])
-  t.is(x.value, 'あがはばは') // transformed hira PA -> HA
+  t.is(x.value, 'あがははぱ') // transformed hira BA -> HA
   t.same(
     x.items.map(tc => tc.weights.get(r)),
-    [0, 0, 0, 0, 1]
+    [0, 0, 0, 1, 0]
   )
 })
 
 test('should expect null', t => {
   let x = new TransformedString(
     'abc',
-    [new RemoveBees(), new HalfVoicedTransform()]
+    [new RemoveBees(), new VoicedTransform()]
   )
   t.is(x.value, 'ac')
 })
